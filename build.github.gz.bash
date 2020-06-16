a476702feda4932003f33d501abdaed5272022e0
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-06-16 19:12:24 -0400

    commit 185013184 on 20200616

--- build.github.gz.bash
+++ build.github.gz.bash
@@ -398,4 +398,4 @@ then	# check if the second argument begins with with the letter c: [[c]url rate]
 else	# process GitHub login
  	_MAINGITHUB_ "$@"
 fi
-# build.github.curl.bash OEF
+# build.github.gz.bash OEF
