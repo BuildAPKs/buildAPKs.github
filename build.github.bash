6d8f86cab580c5304b3da1c2676f6e14080e7ab8
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-06-21 16:26:41 -0400

    commit 629920188 on 20200621

--- build.github.bash
+++ build.github.bash
@@ -237,7 +237,7 @@ _GETREPOS_() {
 _GTGF_ () {	# get git repository
 	NAME="${NAME/#https/git}"
 	RBRANCH="$(git remote show $NAME | grep "HEAD branch" | cut -d ":" -f 2)"
-	printf "%s\\n" "Getting branch$RBRANCH from $NAME:"
+	printf "%s\\n" "Getting branch$RBRANCH from '$NAME'..."
 	( git clone --depth 1 "$NAME" --branch $RBRANCH --single-branch && cd ${NAME##*/} && ( git fsck || _SIGNAL_ "30" "_GTGF_ git fsck" ) && cd $JDR ) || ( cd $JDR && _SIGNAL_ "32" "_GTGF_ git clone" )
 	_IAR_ "$JDR/${NAME##*/}" || _SIGNAL_ "34" "_GTGF_ _IAR_"
 }
