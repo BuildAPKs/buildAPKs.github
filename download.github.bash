81eaad61142547fb82d74dd889e1ae7d9d7c1f09
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-18 09:04:39 -0400

    commit 361242922 on 20200718

--- download.github.bash
+++ download.github.bash
@@ -277,7 +277,7 @@ _MAINGITHUB_ () {
 		touch "$JDR"/profile # create null profile file 
 		touch "$JDR"/repos # create null repos file 
 		printf "\\e[7;38;5;204mUsername %s is found in %s: NOT processing download and build for username %s!  Remove the login from the corresponding file(s) and the account's build directory in %s if an empty directory was created to process %s.  Then run \` %s \` again to attempt to build %s's APK projects, if any.  File %s has more information:\\e[0m\\n" "$USENAME" "~/${RDR##*/}/var/db/[PRXYZ]NAMES" "$USENAME" "~/${RDR##*/}/sources/github/{orgs,users}" "$USENAME" "${0##*/} $USENAME" "$USENAME" "~/${RDR##*/}/var/db/README.md" 
-		awk 'NR>=20 && NR<=46' "$RDR/opt/db/README.md" || _SIGNAL_ "86" "\` awk 'NR>=16 && NR<=46' $RDR/opt/db/README.md \` _MAINGITHUB_"
+		awk 'NR>=20 && NR<=46' "$RDR/opt/db/README.md" || _SIGNAL_ "86" "awk 'NR>=16 && NR<=46' $RDR/opt/db/README.md _MAINGITHUB_"
 		printf "\\e[7;38;5;203m%s is found in %s: NOT processing download and build for username %s!  Remove the username from the corresponding file(s) and the account's build directory in %s if an empty directory was created to process %s.  Then run \` %s \` again to attempt to build %s's APK projects, if any.  Scroll up to read information from the %s file.\\e[0m\\n" "$USENAME" "$(grep -i "^$USENAME$" "$RDR"/var/db/[PRXYZ]NAMES)" "$USENAME" "~/${RDR##*/}/sources/github/{orgs,users}" "$USENAME" "${0##*/} $USENAME" "$USENAME" "~/${RDR##*/}/var/db/README.md" 
 		exit 0
 	else	# check whether login is a user or an organization
@@ -353,20 +353,12 @@ _PRINTJS_ () {
 	printf "\\n\\e[1;34mSearching for C C# C++ Haskell Java* Kotlin Lua Objective-C* Octave Pearl Python R* and Shell language repositories: "'\033]2;Searching for C C# C++ Haskell Java* Kotlin Lua Objective-C* Octave Pearl Python R* and Shell language repositories: OK\007'
 }
 
-_RLREMING_ () { # if connection is available, print GitHub rate limit 
-	# change true to false in file RDR/.conf/DRLIM to disable rate limit check
+_RLREMING_ () { # print GitHub rate limit 
 	if [[ $(awk 'NR==1' "$RDR/.conf/DRLIM") == "true" ]] 
 	then	# get rate limit information from GitHub
-		RATEARRAY=($(curl -is https://api.github.com/rate_limit | grep Rate)) || printf "\\e[2;7;38;5;51m%s\\e[0m\\n\\n" "The Internet connection is not available; Continuing..." # create array with rate information https://developer.github.com/v3/rate_limit/ from GitHub without incurring an API hit
-		if [[ ! -z "${RATEARRAY:-}" ]] # if RATEARRAY is set
-		then	# print GitHub rate limit information to screen
-			printf "%s\\n" "GitHub rate limit information:"
-			printf "\\e[2;7;38;5;144m%s\\e[0m\\n" "${RATEARRAY[0]} ${RATEARRAY[1]}"
-			printf "\\e[2;7;38;5;146m%s\\e[0m\\n" "${RATEARRAY[2]} ${RATEARRAY[3]}"
-			printf "\\e[2;7;38;5;148m%s\\e[0m\\n" "${RATEARRAY[4]} ${RATEARRAY[5]}"
-			[ "$OAUT" != "" ]  && printf "\\e[1;7;38;5;185m%s\\e[0m\\n\\n" "OAUTH token $OAUT is enabled; Continuing..." || printf "\\e[2;7;38;5;150m%s\\e[0m\\n\\n" "File ~/${RDR##*/}/.conf/GAUTH has more information about rate limit; Continuing..." # print information about the RDR/.conf/GAUTH file
-		unset RATEARRAY
-		fi
+		printf "\\e[2;7;38;5;144m%s\\e[0m\\n" "GitHub rate limit information:"
+		printf "%s\\e[0m\\n" "$(curl -is https://api.github.com/rate_limit | grep Ratelimit)"
+		[ "$OAUT" != "" ]  && printf "\\e[2;7;38;5;148m%s\\e[0m\\n\\n" "OAUTH token $OAUT is enabled; Continuing..." || printf "\\e[2;7;38;5;150m%s\\e[0m\\n\\n" "Change true to false in file ~/${RDR##*/}/.conf/DRLIM to disable rate limit check.  File ~/${RDR##*/}/.conf/GAUTH has more information about rate limit; Continuing..." # print information about the RDR/.conf/GAUTH file
 	fi
 }
 
