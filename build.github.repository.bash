3fc9bed5762c6b0f7197d8f0559a64d6b3d3bc56
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-03-31 20:13:01 -0400

    commit 584252790 on 20210331

--- build.github.repository.bash
+++ build.github.repository.bash
@@ -7,13 +7,13 @@ shopt -s nullglob globstar
 export RDR="$HOME/buildAPKs"
 . "$RDR/scripts/bash/shlibs/trap.bash" 67 68 69 "${0##*/} build.github.bash"
 . "$RDR/scripts/bash/init/ushlibs.bash"
-if [ - d "$RDR/sources/github/repos" ]
+if [ -d "$RDR/sources/github/repos" ]
 then
 cd "$RDR/sources/github/repos"
 else
 mkdir -p "$RDR/sources/github/repos"
 cd "$RDR/sources/github/repos"
 fi
-git clone --depth 1 "$@" --single-branch
+git clone --depth 1 "$@" --single-branch || "$RDR/scripts/bash/build/build.in.dir.bash"
 "$RDR/scripts/bash/build/build.in.dir.bash"
 # build.github.repository.bash OEF
