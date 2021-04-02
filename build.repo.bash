d1038dd34245410ac6ec83a477e230c67fa86622
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-04-02 04:58:06 -0400

    commit 146125861 on 20210402

--- build.repo.bash
+++ build.repo.bash
@@ -6,14 +6,11 @@ set -eu
 shopt -s nullglob globstar
 export RDR="$HOME/buildAPKs"
 if [ $# != 1 ]
-then
-printf "\\n%s\\n\\n%s\\n\\n" "The '${0##*/}' command has been tested with https://github.com/search and https://gitlab.com/explore successfully.  Please share addresses to sourcecode at https://github.com/BuildAPKs in order to help develop this topic if you find repo candidates for inclusion." "EXAMPLE USAGE:  '${0##*/} https://github.com/BuildAPKs/buildAPKs.entertainment'" && exit
+then # print help
+	printf "\\n%s\\n\\n%s\\n\\n" "The '${0##*/}' command has been tested with https://github.com/search and https://gitlab.com/explore successfully.  Please share addresses to sourcecode at https://github.com/BuildAPKs in order to help develop this topic if you find repo candidates for inclusion." "EXAMPLE USAGE:  ${0##*/} https://github.com/BuildAPKs/buildAPKs.entertainment" && exit
 fi
 _CLONEBUILD_() {
-cd "$RDR/sources/$SITENAME/$LOGINAME"
-git clone --depth 1 "$@" --single-branch 
-cd "$REPONAME"
-"$RDR/scripts/bash/build/build.in.dir.bash"
+	cd "$RDR/sources/$SITENAME/$LOGINAME" && git clone --depth 1 "$@" --single-branch && cd "$REPONAME" && "$RDR/scripts/bash/build/build.in.dir.bash"
 }
 BASENAME="${@%/}" # strip trailing slash
 BASENAME="${BASENAME#*//}" # strip before double slash
@@ -21,16 +18,22 @@ REPONAME="${BASENAME##*/}" # strip before last slash
 LOGINAME="${BASENAME%/*}" # strip after last slash
 LOGINAME="${LOGINAME##*/}" # strip before last slash
 SITENAME="${BASENAME%%/*}" # strip after first slash
+if [ ! -n "${LOGINAME:-}" ] || [ ! -n "${REPONAME:-}" ] || [ ! -n "${SITENAME:-}" ] # any variable or more is empty
+then # exit
+	printf "%s\\n" "EXCEPTION processing $@ in directory ~/${RDR##*/}/sources/$SITENAME/$LOGINAME/$REPONAME:  EXITING..."
+	exit 101
+fi
 printf "%s\\n" "Processing $@ in directory ~/${RDR##*/}/sources/$SITENAME/$LOGINAME/$REPONAME:"
+sleep 1.6
 if [ -d "$RDR/sources/$SITENAME/$LOGINAME/$REPONAME" ]
 then
-cd "$RDR/sources/$SITENAME/$LOGINAME/$REPONAME"
-"$RDR/scripts/bash/build/build.in.dir.bash"
+	cd "$RDR/sources/$SITENAME/$LOGINAME/$REPONAME"
+	"$RDR/scripts/bash/build/build.in.dir.bash"
 elif [ -d "$RDR/sources/$SITENAME/$LOGINAME" ]
 then
-_CLONEBUILD_ "$@"
+	_CLONEBUILD_ "$@"
 else
-mkdir -p "$RDR/sources/$SITENAME/$LOGINAME"
-_CLONEBUILD_ "$@"
+	mkdir -p "$RDR/sources/$SITENAME/$LOGINAME"
+	_CLONEBUILD_ "$@"
 fi
 # build.repo.bash OEF
