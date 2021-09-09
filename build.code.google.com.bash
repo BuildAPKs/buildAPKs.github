26506b6e402fee06e205090e00db8a1ca88243d1
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-09-09 19:54:37 -0400

    commit 554985383 on 20210909

--- build.code.google.com.bash
+++ build.code.google.com.bash
@@ -5,7 +5,7 @@
 set -eu
 shopt -s nullglob globstar
 export RDR="$HOME/buildAPKs"
-if [ $# != 1 ]
+if [ $# = 0 ]
 then # print help
 	printf "\\n%s\\n\\n%s\\n%s\\n%s\\n\\n%s\\n\\n" "The ${0##*/} command has been tested with https://code.google.com successfully.  Share addresses to sourcecode at https://github.com/BuildAPKs in order to help develop this software if you find APK repo candidates for inclusion." "EXAMPLE USAGES:" "	${0##*/} https://code.google.com/p/apps-for-android" "	${0##*/} https://code.google.com/archive/p/permission-explorer" "If you find repo candidates for inclusion, please share addresses to sourcecode at https://github.com/BuildAPKs/db.BuildAPKs in order to help develop this software.  Thank you for using ${0##*/};  Enjoy🎵🎶"
 	exit
